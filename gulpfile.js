9efe6552422d57a51c8097bb69cbcd74c24b2729
James Lockwood <jalockwo@gmail.com>
2017-02-27 23:27:23 +0000

    meh, service worker fail.

--- gulpfile.js
+++ gulpfile.js
@@ -9,25 +9,24 @@ const webpackConfig = require('./webpack.config.js');
 gulp.task('build', ['clean'], () => gulp
     .src('')
     .pipe(webpackStream(webpackConfig, webpack))
-    .pipe(gulp.dest('dist/')));
+    .pipe(gulp.dest('dist/')
+));
 
 gulp.task('server', () => {
-  const config = Object.create(webpackConfig);
-  config.devtool = 'eval';
-  config.entry.app.unshift(
-    'webpack-dev-server/client?http://localhost:8080/' // inline reloads
-  );
-  new WebpackDevServer(webpack(config))
-    .listen(8080, 'localhost', (err) => {
-      if (err) {
-        throw new gutil.PluginError('webpack-dev-server', err);
-      }
-    });
+    const config = Object.create(webpackConfig);
+    config.devtool = 'eval';
+    config.entry.app.unshift('webpack-dev-server/client?http://localhost:8080/');
+    new WebpackDevServer(webpack(config))
+        .listen(8080, 'localhost', (err) => {
+            if (err) {
+                throw new gutil.PluginError('webpack-dev-server', err);
+            }
+        });
 });
 
 gulp.task('clean', () => del([
-  'dist/**/*'
-  // '!dist/foo.json'
+    'dist/**/*'
+    // '!dist/foo.json'
 ]));
 
 gulp.task('default', ['build']);

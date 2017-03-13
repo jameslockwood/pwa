73a2cabc18e8500917e7e248ba4e47292f63cc62
James Lockwood <jalockwo@gmail.com>
2017-03-13 11:01:47 +0000

    adding faker to create names

--- localhost/mocks/people.js
+++ localhost/mocks/people.js
@@ -1,51 +1,16 @@
-module.exports = [
-    {
-        name: 'James Johnson',
-        age: 31,
-        children: false,
-        id: 0,
+const faker = require('faker'); // eslint-disable-line import/no-extraneous-dependencies
+
+const people = [];
+
+for (let i = 0; i < 10; i++) {
+    const age = Math.floor(Math.random() * 90);
+    people.push({
+        name: faker.name.findName(),
+        age,
+        children: age > 16 && Math.random() > 0.5,
+        id: i,
         local: false
-    },
-    {
-        name: 'Marilyn Woods',
-        age: 40,
-        children: false,
-        id: 1,
-        local: false
-    },
-    {
-        name: 'John Smith',
-        age: 72,
-        children: true,
-        id: 2,
-        local: false
-    },
-    {
-        name: 'Susan David',
-        age: 61,
-        children: true,
-        id: 3,
-        local: false
-    },
-    {
-        name: 'William Woolley',
-        age: 35,
-        children: true,
-        id: 4,
-        local: false
-    },
-    {
-        name: 'Jo Peters',
-        age: 33,
-        children: true,
-        id: 5,
-        local: false
-    },
-    {
-        name: 'Trevor Isaksson',
-        age: 1,
-        children: false,
-        id: 6,
-        local: false
-    }
-];
+    });
+}
+
+module.exports = people;

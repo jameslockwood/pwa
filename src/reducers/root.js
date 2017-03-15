b6dcad49e19dec86b4cf9a8f8270dfe351841b44
James Lockwood <jalockwo@gmail.com>
2017-03-15 16:23:11 +0000

    slight renaming

--- src/reducers/root.js
+++ src/reducers/root.js
@@ -3,14 +3,14 @@
 import type { RootState, Action } from 'src/types/core';
 import boot from './boot';
 import people from './people/root';
-import filterString from './filter';
-import parentsOnly from './parents';
+import filter from './filter';
+import parents from './parents';
 
 export default function reducer(state: Object = {}, action: Action): RootState {
     return {
         boot: boot(state.boot, action),
         people: people(state.people, action),
-        filterString: filterString(state.filterString, action),
-        parentsOnly: parentsOnly(state.parentsOnly, action)
+        filterString: filter(state.filterString, action),
+        parentsOnly: parents(state.parentsOnly, action)
     };
 }

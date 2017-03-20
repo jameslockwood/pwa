10997c7f1b39fa4c54b868a9426c1e4e422f1e0e
James Lockwood <jalockwo@gmail.com>
2017-03-20 11:37:54 +0000

    adding basic manifest

--- src/shell.js
+++ src/shell.js
@@ -1,13 +1,14 @@
 import './assets/shell.less';
-import serviceWorkerReg from './service-worker-reg';
+import serviceWorkerReg from './utils/service-worker-reg';
 
 // Application loading flow:
 // 1 - critical path styles and javascript are loaded here to allow page to render quickly.
-// 2 - main app then loaded asynchronously
-// 3 - app then loads various views / features asynchronously
+// 2 - service worker is installed (if not already) 
+// 3 - main app then loaded asynchronously
+// 4 - app then loads various views / features asynchronously
 
 if (process.env.NODE_ENV === 'production') {
-    serviceWorkerReg();
+    serviceWorkerReg('service-worker.js');
 }
 
 const removeOverlay = () => {
@@ -20,3 +21,8 @@ import('./app').then((app) => {
     const hostElement = document.getElementById('app-bootstrap');
     app.boot(hostElement).then(removeOverlay);
 });
+
+// handle when user is online/offline here
+const updateOnlineStatus = () => console.log(`user is ${navigator.onLine ? 'online' : 'offline'}`);
+window.addEventListener('online', updateOnlineStatus);
+window.addEventListener('offline', updateOnlineStatus);

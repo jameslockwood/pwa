25ec8da05aeb2dfaa417807b1abe6db724783849
James Lockwood <jalockwo@gmail.com>
2017-03-31 10:19:46 +0100

    preloading assets

--- tooling/webpack.html.js
+++ tooling/webpack.html.js
@@ -5,12 +5,21 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 const StyleExtHtmlWebpackPlugin = require('style-ext-html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const PreloadWebpackPlugin = require('preload-webpack-plugin');
 
 const shellEntry = 'shell.html';
 const commonHtmlPlugins = [
     // 'asyncs' our initial shell scripts
     new ScriptExtHtmlWebpackPlugin({
         defaultAttribute: 'async'
+    }),
+
+    // preloads our chunk scripts
+    // https://www.smashingmagazine.com/2016/02/preload-what-is-it-good-for/
+    new PreloadWebpackPlugin({
+        rel: 'preload',
+        as: 'script',
+        include: 'asyncChunks'
     })
 ];
 
